1658252f7a8999d4856206d5e6fcde33d95cb100
Dimasku <dirodriguez@pragmaconsultores.com>
2015-12-15 11:20:39 -0300

    15/12/2015 - 11:20

---
+++ Web Api1/App_Start/PersonDBInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Web_Api1.Models;
+
+namespace Web_Api1.App_Start
+{
+    public class PersonDBInitializer : DropCreateDatabaseAlways<PersonContext>
+    {
+        protected override void Seed(PersonContext context)
+        {
+            var persons = new List<Person> {
+                new Person {  Name = "Julio", LastName ="Gonzales", Mail = "Mail1"},
+                new Person {Name = "Pedro", LastName = "Rodriguez", Mail = "Mail2" },
+                new Person {Name = "Diego", LastName = "Skupnjak", Mail ="Mail3" },
+                new Person {Name = "Pablo", LastName ="Martinez", Mail ="Mail4" }
+            };
+            persons.ForEach(c => context.Personas.Add(c));
+            context.SaveChanges();
+        }
+
+    }
+}
---
+++ Web Api1/Models/PersonContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Web_Api1.Models
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> Personas { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
